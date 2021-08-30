cedee1ffe648d776cb5cc8c8c7ad3a185867c0cf
Matt Hill <hill2420@gmail.com>
2021-08-30 16:56:59 -0400

    fight, skip, wrong word

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,13 +14,13 @@ var fight = function() {
     // Alert players that they starting the round
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt(" Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose. ");
+    if (promptFight === "fight" || promptFight === "FIGHT") {
     //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
     enemyHealth = enemyHealth - playerAttack;
     // Log a resulting message to the console so we know that it worked.
     console.log(
     playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining "
     );
-
     //check enemy health
     if (enemyHealth <= 0) {
         window.alert(enemyName + " has died ");
@@ -40,6 +40,13 @@ var fight = function() {
     else {
         window.alert(playerName + " still has " + playerHealth + " health left ");
     }
+
+} else if (promptFight === "skip" || promptFight === "SKIP") {
+    window.alert(playerName + " has chosen to skip the fight!");
+} else {
+    window.alert(" You need to choose a valid option. Try Again!");
+}
+
 };
 
 fight();
