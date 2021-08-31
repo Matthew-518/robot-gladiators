114def3ca50ea36212718cca61e8fb5076bb30c2
Matt Hill <hill2420@gmail.com>
2021-08-31 13:01:50 -0400

    console problems

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,8 +14,7 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    // Alert players that they starting the round
-    window.alert("Welcome to Robot Gladiators!");
+    while(enemyHealth > 0) {
     var promptFight = window.prompt(" Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose. ");
 if (promptFight === "fight" || promptFight === "FIGHT") {
     //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
@@ -56,22 +55,13 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     }else {
     window.alert(" You need to choose a valid option. Try Again!");
     }
+}
 };
 
-for( var i = 0; i < enemyNames.length; i++) {
+for(var i = 0; i < enemyNames.length; i++) {
     fight(enemyNames[i]);
 }
 
-//for(var i = 0; i < enemyNames.length; i++) {
-    //console.log(enemyNames[i]);
-    //console.log(i);
-    //console.log(enemyNames[i] + " is at " + i + " index ");
-//}
-
-
-// You can also log multiple values at once like this
-
-//console.log(playerNames, playerAttack, playerHealth);
 
 
 
