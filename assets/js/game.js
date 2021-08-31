590a039b7eee867e2783f00c7217799f7a23e716
Matt Hill <hill2420@gmail.com>
2021-08-31 18:04:18 -0400

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -57,6 +57,13 @@ var fight = function(enemyName) {
   }
 };
 
+//function to start a new game
+var startGame = function() {
+        // reset player stats
+        playerHealth = 100;
+        playerAttack = 10;
+        playerMoney = 10;
+     
 for (var i = 0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
         // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
@@ -70,11 +77,38 @@ for (var i = 0; i < enemyNames.length; i++) {
         fight(pickedEnemyName);
     }
 
+
    else {
        window.alert("You have lost your robot in battle! Game Over!");
        break;
    }
 }
+endGame();
+};
+
+
+var endGame = function() {
+    window.alert("The game has now ended. Let's see how you did!")
+    if (playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    }
+    else {
+        window.alert("You've lost your robot in battle");
+    }
+
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+      // restart the game
+      startGame();
+    } 
+    else {
+      window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    } 
+}
+startGame();
+
+
 
  //if (promptFight === "fight" || promptFight === "FIGHT") {
 
